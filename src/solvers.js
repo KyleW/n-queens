6c95039025ec1fd32784e105f4dbf93d9159f87a
Hack Reactor Students <kyle.warneck@gmail.com>
2013-11-20 16:59:59 -0800

    Initial version of findNQueens . It passes tests, but it's clearly not working.

--- src/solvers.js
+++ src/solvers.js
@@ -55,8 +55,57 @@ window.countNRooksSolutions = function(n, solutionCount){
 
 
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n queens placed such that none of them can attack each other
+
+//n has to be => 4 for this to work
+
 window.findNQueensSolution = function(n){
-  var solution = undefined; //fixme
+  //make an n x n board
+  var board = new Board({n: n});
+
+  // Make an array of all possible spaces
+  var availableSpaces = [];
+
+  for ( var i = 0;  i < n; i++ ) {
+    for (var j = 0; j < n; j++){
+      availableSpaces.push([i,j]);
+    }
+  }
+
+  // put a queen on the first space
+  var placeQueen = function() {
+    var currentMove = availableSpaces.shift();
+    board.togglePiece(currentMove[0], currentMove[1]);
+
+    // remove all spaces from the array that now have conflicts
+    // remove rows
+    for(var k = 0 ; k < availableSpaces.length; k++){
+      //remove spaces in the same column, row, major and minor diagonal
+      if (
+        availableSpaces[k][0] === currentMove[0] || //columns
+        availableSpaces[k][1] === currentMove[1] || //rows
+        (currentMove[0]- availableSpaces[k][0]) === (currentMove[1] - availableSpaces[k][1]) || // major diagonal
+        (currentMove[0] - availableSpaces[k][0]) === (availableSpaces[k][1] - currentMove[1])   //  //minor diagonal
+        ) {
+        availableSpaces.splice(k ,1);    //remove available space[i]
+      }
+    }
+  };
+
+  var counter = 0;
+
+  while (availableSpaces.length > 0){
+    // place the next piece at the first space in the array
+    // repeat until array is empty
+    placeQueen();
+    counter++;
+  }
+
+
+  if (n > 3 && counter !== n){
+    //then start again starting on the second space
+  }
+
+  var solution = board.attributes;
 
   console.log('Single solution for ' + n + ' queens:', JSON.stringify(solution));
   return solution;
