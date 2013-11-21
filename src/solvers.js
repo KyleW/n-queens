e9d398e1c18d54aac8b47f1bcb2854ee5086a861
Hack Reactor Students <kyle.warneck@gmail.com>
2013-11-20 19:56:17 -0800

    findNQueens in progress

--- src/solvers.js
+++ src/solvers.js
@@ -125,11 +125,101 @@ window.findNQueensSolution = function(n){
   return solution;
 };
 
+//  FROM DATA STRUCTURES SPRINT
+
+// var makeTree = function(value){
+//   var newTree = Object.create(treeMethods);
+//   newTree.value = value;
+//   newTree.children = [];
+//   newTree.parent= null;
+//   return newTree;
+// };
+
+// var treeMethods = {};
+
+// treeMethods.addChild = function(value){
+//   var temp = makeTree(); // generate a new tree
+//   temp.value = value;
+//   this.children.push(temp);
+//   temp.parent= this;
+// };
+
+// treeMethods.depthFirstLog  = function(fn) {
+//   fn.call(this, arguments);
+//   if (this.children.length > 0) {
+//     for (var i = 0; i< this.children.length; i++) {
+//       this.children[i].depthFirstLog(fn);
+//     }
+//   }
+// };
+
 
 // return the number of nxn chessboards that exist, with n queens placed such that none of them can attack each other
-window.countNQueensSolutions = function(n){
-  var solutionCount = undefined; //fixme
 
-  console.log('Number of solutions for ' + n + ' queens:', solutionCount);
-  return solutionCount;
+window.countNQueensSolutions = function(n){
+
+  var allPossible = function() {
+    var availableSpaces = [];
+    for ( var i = 0;  i < n; i++ ) {
+      for (var j = 0; j < n; j++){
+        availableSpaces.push([i,j]);
+      }
+    }
+
+    return availableSpaces;
+  }();
+
+  console.log(allPossible);
+
+  var makeNode = function(value){
+    var newNode = Object.create(nodeMethods);
+    newNode.value = value;
+    newNode.children = [];
+    newNode.parent= null;
+    newNode.availableSpaces = allPossible;
+    newNode.level = 0;
+    return newNode;
+  };
+
+  var nodeMethods = {};
+
+  nodeMethods.addChild = function(value){
+    var temp = makeNode(); // generate a new tree
+    temp.value = value;
+    temp.parent= this;
+    temp.level = this.level + 1;
+    temp.availableSpaces = removeConflicts(this.availableSpaces , temp.value);
+    this.children.push(temp);
+  };
+
+  // function that takes a list of available spaces and given a move subtracts all 
+  var removeConflicts = function(availableSpaces,currentMove){
+    //available spaces is an array of what's there before you make your move
+    //currentMove is the move you're making right now
+    for(var k = availableSpaces.length - 1 ; k >= 0; k--){
+      //remove spaces in the same column, row, major and minor diagonal
+      if (
+        availableSpaces[k][0] === currentMove[0] || //columns
+        availableSpaces[k][1] === currentMove[1] || //rows
+        (currentMove[0]- availableSpaces[k][0]) === (currentMove[1] - availableSpaces[k][1]) || // major diagonal
+        (currentMove[0] - availableSpaces[k][0]) === (availableSpaces[k][1] - currentMove[1])   //  //minor diagonal
+        ) {
+        availableSpaces.splice(k ,1);    //remove available space[i]
+      }
+    }
+    //returns an array
+    return availableSpaces;
+  };
+
+  var tree = makeNode();
+  for (var i = 0 ; i < n ; i++){
+    tree.addChild([0,i]);
+  }
+
+  console.log(tree);
+
+  // var solutionCount = undefined; //fixme
+
+  // console.log('Number of solutions for ' + n + ' queens:', solutionCount);
+  // return solutionCount;
 };
